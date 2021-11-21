a2399c79674a1814c648e29db04d88d528330777
surprizetallboi <christiannchrist@gmail.com>
2021-11-21 00:31:10 -0600

    1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Button } from "./Button.jsx"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <h2>it's nice to see you here</h2>
+      </header>
+      <br />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
