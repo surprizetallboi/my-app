9994d0f895ebab19d6ac2bddf39335f03f6936e4
surprizetallboi <christiannchrist@gmail.com>
2021-11-29 23:26:58 -0600

    11/29 updates

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,17 @@ function App() {
     <div className="App">
       <button className={isShrunk? "butt1" : "butt2"} onClick={ ()=> setIsShrunk(isShrunk => !isShrunk)}>Click to begin</button>
       <header className={isShrunk? "appHead1" : "appHead2"}>
-        {/* <p>isShrunk: {isShrunk.toString()}</p> */}
         <h2 id="hello">hey, my name is</h2>
         <h1>CHRISTIAN DANIEL</h1>
         <h2 id="make">I make stuff and do things</h2>
       </header>
       <br />
       <br />
-      <p className={isShrunk? "buttonText1" : "buttonText2"} >Press anywhere to begin</p>
-      {/* <Button /> */}
-      
-      
+      <br />
+      <br />
+      <br />
+      <br />
+      <p className={isShrunk? "buttonText1" : "buttonText2"} >Press anywhere to begin</p>      
       {/* <Circle /> */}
     </div>
   );
